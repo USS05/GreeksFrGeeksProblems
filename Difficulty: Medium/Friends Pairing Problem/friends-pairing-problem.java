0f067c22fc479e981b650892cc26322ce926ef28
USS05 <umasatyasaibollam@gmail.com>
2026-07-06 15:43:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Friends Pairing Problem/friends-pairing-problem.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long countFriendsPairings(int n) {
+        // code here
+        //Base-Case:
+        if(n==1 || n==2)
+        {
+            return n;
+        }
+        return countFriendsPairings(n-1)+(n-1)*countFriendsPairings(n-2);
+        
+    }
+}
